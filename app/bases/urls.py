d457d994c2e3b80c5d36af74dd161740f2097047
marlon <falconsoft.3d@gmail.com>
2019-08-24 01:01:58 -0400

    51 Ruta para Login

--- app/bases/urls.py
+++ app/bases/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from bases.views import Home
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',Home.as_view(), name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='bases/login.html'),
+    name = 'login')
 ]
