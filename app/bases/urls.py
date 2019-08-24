3900e0783c506f6d7ac60198caacb8c9766fcbe3
marlon <falconsoft.3d@gmail.com>
2019-08-24 01:10:28 -0400

    52 Logout

--- app/bases/urls.py
+++ app/bases/urls.py
@@ -5,5 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('',Home.as_view(), name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='bases/login.html'),
-    name = 'login')
+    name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='bases/login.html'),
+    name = 'logout')
 ]
