ab6e79c362f6332e04aca5b6829d27f552be03a5
marlon <falconsoft.3d@gmail.com>
2019-08-24 01:19:30 -0400

    U9 Catalogos: 55: Herencia de Modelos

--- app/bases/models.py
+++ app/bases/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ClaseModelo(models.Model):
+    estado = models.BooleanField(default=True)
+    # Fecha de Creacion
+    fc = models.DateTimeField(auto_now_add=True)
+    # Fecha de Modificación
+    fm = models.DateTimeField(auto_now=True)
+    # Usuario que crea
+    uc = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Usuario Modifica
+    um = models.IntegerField(null=True, blank=True)
+
+
+    # Decimos que es una clase abstracta para que Django no cree las migraciones
+    class Meta:
+        abstract=True
