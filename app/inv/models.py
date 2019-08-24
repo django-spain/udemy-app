1797d6254e199865077a5099842b424d628a79e5
marlon <falconsoft.3d@gmail.com>
2019-08-24 01:23:34 -0400

    U9 Catalogos: 56: Categoria de Modelos

--- app/inv/models.py
+++ app/inv/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from bases.models import ClaseModelo
 
-# Create your models here.
+class Categoria(ClaseModelo):
+    descripcion = models.CharField(
+        max_length=100,
+        help_text = 'Descripción de la categoría',
+        unique=True
+    )
+
+    def __stf__(self):
+        return '{}'.format(self.descripcion)
+
+    # Ponemos en mayuscula al guardar
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(Categoria, self)
+
+
+    class Meta:
+        verbose_name_plural = "categorias"
